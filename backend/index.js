a501a013643d741671f0e4c8b049906970a71c4d
Aniket <108399480+Aniket2021448@users.noreply.github.com>
2023-07-30 23:08:11 +0530

    updating 1

--- backend/index.js
+++ backend/index.js
@@ -76,13 +76,11 @@ app.get("/" , (req, res)=>{ //by default API. sending this text.
     res.send("Server is running")
 })
 
-// Serve static assets from the React client build folder
-app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+// Serve the main index.html for all routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
 });
-
 
 //sign up
 app.post("/signup", async (req, res) => {
